5a8ec5b9cd287c06cdfb1362276259780374cec3
jgarcia3htp <jgarcia@3htp.com>
2024-12-27 08:24:15 -0500

    latest

--- config.sh
+++ config.sh
@@ -19,13 +19,13 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 #minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-#sudo su root
-#minikube start --force
+sudo su root
+minikube start --force
 
 #granafa y proxy inverso
-curl -O https://raw.githubusercontent.com/main/nisanchezva/monitoring.yaml
-curl -O https://raw.githubusercontent.com/main/nisanchezva/Dockerfile_nginx
-curl -O https://raw.githubusercontent.com/main/nisanchezva/deploy.sh
-curl -O https://raw.githubusercontent.com/main/nisanchezva/nginx.conf
-#sh deploy.sh
-#su ubuntu
+curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/monitoring.yaml
+curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/Dockerfile_nginx
+curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/deploy.sh
+curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/nginx.conf
+sh deploy.sh
+su ubuntu
