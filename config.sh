314f79a88480716b13680cad61b40295665f62c4
jgarcia3htp <jgarcia@3htp.com>
2024-12-27 07:41:14 -0500

    latest

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 #actualizacion de paquetes
 sudo apt-get update
@@ -16,15 +16,15 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
-sudo su root
+#sudo su root
 
-minikube start --force
+#minikube start --force
 
 #granafa y proxy inverso
 curl -O https://raw.githubusercontent.com/main/nisanchezva/monitoring.yaml
 curl -O https://raw.githubusercontent.com/main/nisanchezva/Dockerfile_nginx
 curl -O https://raw.githubusercontent.com/main/nisanchezva/deploy.sh
 curl -O https://raw.githubusercontent.com/main/nisanchezva/nginx.conf
-sh deploy.sh
+#sh deploy.sh
 
-su ubuntu
+#su ubuntu
