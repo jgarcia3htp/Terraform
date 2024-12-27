feb271bc7c2bc5f4d5c484a8dd8d81a69b6c290f
jgarcia3htp <jgarcia@3htp.com>
2024-12-27 08:04:37 -0500

    latest

--- config.sh
+++ config.sh
@@ -8,6 +8,10 @@ sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
+#directorio de trabajo
+sudo mkdir /workdir
+cd /workdir/
+
 #kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -15,9 +19,7 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 #minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-
 #sudo su root
-
 #minikube start --force
 
 #granafa y proxy inverso
@@ -26,5 +28,4 @@ curl -O https://raw.githubusercontent.com/main/nisanchezva/Dockerfile_nginx
 curl -O https://raw.githubusercontent.com/main/nisanchezva/deploy.sh
 curl -O https://raw.githubusercontent.com/main/nisanchezva/nginx.conf
 #sh deploy.sh
-
 #su ubuntu
--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "terraform-server" {
     ami = "ami-0e2c8caa4b6378d8c"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
 
     user_data = "${file("config.sh")}"
     
