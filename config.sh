be5e6c220c0d0a68d995c12dc1c3aa9474327416
jgarcia3htp <jgarcia@3htp.com>
2024-12-27 14:12:21 -0500

    latest

--- config.sh
+++ config.sh
@@ -29,6 +29,12 @@ curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/deploy
 curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/nginx.conf
 curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/dashboard.json
 
+#aplicacion .war
+curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/tomcat-deploy.yaml
+curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/tomcat-hpa.yaml
+curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/tomcat-pvc.yaml
+curl -O https://raw.githubusercontent.com/nisanchezva/practicaGrupal/main/tomcat-svc.yaml
+
 #ejecutables iniciales
 sudo sh /workdir/deploy.sh
 sudo minikube start --force
@@ -38,3 +44,7 @@ sudo kubectl config use-context minikube
 sudo kubectl create namespace monitoring
 sudo kubectl create configmap grafana-dashboard -n monitoring --from-file=./dashboard.json
 sudo kubectl apply -f monitoring.yaml
+sudo kubectl apply -f tomcat-deploy.yaml
+sudo kubectl apply -f tomcat-svc.yaml
+sudo kubectl apply -f tomcat-pvc.yaml
+sudo kubectl apply -f tomcat-hpa.yaml
